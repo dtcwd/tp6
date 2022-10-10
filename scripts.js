40d930acb4358ad94651fe0e402d0aa90d272aa2
Coby <coby.nelson@wsu.edu>
2022-10-10 04:14:04 +0000

    updated with AJAX calls

--- scripts.js
+++ scripts.js
@@ -1,6 +1,34 @@
-// tp5 java script 
+// tp6 java script 
+// function to load files from a url "fromfile" into object identified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
 
 window.onload = function() {
+  
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
   
   document.querySelector("#pagebanner h1").classList.add("tp");
   
@@ -25,4 +53,7 @@ window.onload = function() {
   }
   
   document.querySelector("#columns #equipment ul").innerHTML += "<li>Electric Mixer</li>";
+  
+  
+  
 }// end window onload 
